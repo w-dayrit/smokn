b37135209518ae6aec77fd0b488e270afac65725
william dayrit <williamdayritis@gmail.com>
2015-08-15 21:01:45 -0700

    create controller folder. refactor index router to use it

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var indexController = requires('../controllers/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', indexController.renderIndex);
+
+
 
 module.exports = router;
