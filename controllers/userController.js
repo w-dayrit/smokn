6ca3bb7c124a7e0a601a92104030f6a8112326e7
bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) bash (wget -qO- https://raw.githubusercontent.com/ga-instructors/installfest_script/master/builds/ubuntu) <jacklee0223@gmail.com>
2015-08-17 13:58:50 -0700

    major design change, merged login into landing page

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ var User = require('../models/User');
 
 // GET '/' - renders index page
 module.exports.renderUserIndex = function(req, res, next) {
-  res.render('users/index', {title: 'SMOKN'});
+  res.render('landingpage', {title: 'SMOKN'});
 };
 
 module.exports.renderUserNew = function(req, res, next) {
@@ -21,5 +21,3 @@ module.exports.renderUserCreate = function(req, res, next) {
     res.redirect('/users/' + user.id);
   })
 }
-
-
